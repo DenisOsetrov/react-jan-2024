5940f3dfea6fadd2fd4423bdd8f560610b671aab
DenisOsetrov <dvosetrov@gmail.com>
2024-05-04 00:51:24 +0300

    hw-1, not css

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -5,29 +5,53 @@ export interface IProductProps {
     title: string,
     description: string,
     price: number,
-    discountPercentage: number,
+    discountPercentage?: number,
     rating: number,
     stock: number,
     brand: string,
     category: string,
-    thumbnail: string,
+    thumbnail?: string,
     images: string[]
 }
 
 // пропишемо і протипізуємо children (частина коду, що знаходиться між відкритим і закритим тегами компонента)
-type ITypeProductProps = IProductProps & {children?: React.ReactNode};
-const Product: FC<ITypeProductProps> = ({id, title, price, images, brand, category, discountPercentage, rating, stock, thumbnail, description, children}) => {
+export type ITypeProductProps = IProductProps & {children?: React.ReactNode};
+const Product: FC<ITypeProductProps> = ({
+                                            id,
+                                            title,
+                                            price,
+                                            images,
+                                            brand,
+                                            category,
+                                            discountPercentage,
+                                            rating,
+                                            stock,
+                                            thumbnail,
+                                            description}) => {
+
+    // Обчислення нової ціни з урахуванням знижки
+    const newPrice: number = price - price * (discountPercentage || 0) / 100;
+
     return (
         <div>
-            <h2>{id}. {title} - {price}</h2>
+            <h2>{id}. {title}. Old price - {price}$.</h2>
+            <h2>New price on this week - {newPrice}$.</h2>
+            <p> Discount - {discountPercentage}%. </p>
+            <h4> Description: </h4>
+            <p> Brand - {brand} </p>
+            <p> Category - {category} </p>
+            <p> {description} </p>
+                        {/*Відображення кожного зображення з масиву images за допомогою методу map().*/}
+                        {/*Кожне зображення має унікальний ключ, що дорівнює індексу масиву*/}
+            <ul>
+                {images.length > 0 && images.map((image, index) => <li><img key={index} src={image} alt={title}/></li>)}
+            </ul>
+            <p> Thumbnail - {thumbnail} </p>
+            <p> Rating - {rating} </p>
+            <p> Stock - {stock} units</p>
 
-            // Відображення кожного зображення з масиву images за допомогою методу map().
-            // Кожне зображення має унікальний ключ, що дорівнює індексу масиву,
-            // та властивість alt, яка містить заголовок продукту.
 
-            {images.length > 0 && images.map((image, index) => (
-                <img key={index} src={image} alt={title} />
-            ))}
+
         </div>
     );
 };
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,21 +1,24 @@
-import React, {FC} from 'react';
-import {IProductProps} from "../product/Product";
+import React, {FC, useEffect, useState} from 'react';
+import Product, {IProductProps} from "../product/Product";
 
 
 const Products: FC = () => {
 
-let product : IProductProps[]
+    const [products, setProduct] = useState<IProductProps[]>([]);
+
+useEffect(() => {
     fetch('https://dummyjson.com/products')
         .then(value => value.json())
-        .then(value => {
-            product = value.product;
-            console.log(product)
+        .then(({products}) => {
+            setProduct(products)
         });
-    console.log('test');
+}, []);
+
 
     return (
         <div>
-
+            {products.map(({id, title, description, price, images, category, brand, rating, stock,thumbnail,discountPercentage}) => (
+                <Product key={id} id={id} title={title} description={description} price={price} images={images} category={category} brand={brand} discountPercentage={discountPercentage} rating={rating} stock={stock} thumbnail={thumbnail} />))}
         </div>
     );
 };
