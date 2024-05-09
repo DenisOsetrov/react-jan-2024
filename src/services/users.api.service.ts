a35b2f39578c1a7a48ec8722ce4238280d41820b
DenisOsetrov <dvosetrov@gmail.com>
2024-05-09 10:33:14 +0300

    hw-2, 2 part users-user-post

--- src/services/users.api.service.ts
+++ src/services/users.api.service.ts
@@ -11,6 +11,9 @@ let axiosInstance = axios.create({
 // беремо axiosInstance - з базовою url - і додаємо /users - для отримання всіє юзерів
 const getAllUsers = ():Promise <AxiosResponse<UserModel[]>> => {
     return axiosInstance.get('/users')
+    //     .then(value => value.data) // !!! Це важливо! Тут ми дістаємо 1 об'єкт з багатьох, тому після
+    // // розкоментування треба змінювати компоненту UsersComponent, бо там ми отримуємо від axios декілька об'ктів і
+    // // вже тоді з ними працюємо
 }
 
 
