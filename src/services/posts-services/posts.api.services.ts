4c5ed7c3efde123e58f2a20f8c388764417edf86
DenisOsetrov <dvosetrov@gmail.com>
2024-05-09 10:28:30 +0300

    hw-2, 2 part users-user-post

--- src/services/posts-services/posts.api.services.ts
+++ src/services/posts-services/posts.api.services.ts
@@ -9,4 +9,16 @@ export {
     getAllPostsByUserId
 }
 
-// перевести в Аxios!!!! + CSS
+
+// // axiosInstance - стандартна назва базового об'єкта.
+// const axiosInstance = axios.create ({
+//     baseURL: 'https://dummyjson.com',
+//     headers: {'Content-Type': 'application/json'}
+// })
+//
+// const getAllUsers = (): Promise<UsersResponseModel> => {
+//     return axiosInstance.get('/users')
+//         .then(response => response.data); // axios повертає багато об'єктів, тому вказуємо, лише на потрібний - data!
+// };
+// // + import
+// // перевести в Аxios!!!! + CSS
--- src/services/users-services/users.api.services.ts
+++ src/services/users-services/users.api.services.ts
@@ -1,21 +1,22 @@
 import {UsersResponseModel} from "../../models/response-models/UsersResponseModel";
+import axios from "axios";
 
 
-const  getAllUsers = (): Promise<UsersResponseModel> => {
-    return fetch('https://dummyjson.com/users')
-        .then(value => value.json())
-}
-
-// // axiosInstance - стандартна назва базового об'єкта.
-// const axiosInstance = axios.create ({
-//     baseURL: 'https://dummyjson.com',
-//     headers: {'Content-Type': 'application/json'}
-// })
-//
-// const getAllUsers = (): Promise<AxiosResponse<UsersResponseModel>> => {
-//     return axiosInstance.get('/users')
+// const  getAllUsers = (): Promise<UsersResponseModel> => {
+//     return fetch('https://dummyjson.com/users')
+//         .then(value => value.json())
 // }
-// + import
+
+// axiosInstance - стандартна назва базового об'єкта.
+const axiosInstance = axios.create ({
+    baseURL: 'https://dummyjson.com',
+    headers: {'Content-Type': 'application/json'}
+})
+
+const getAllUsers = (): Promise<UsersResponseModel> => {
+    return axiosInstance.get('/users')
+        .then(response => response.data); // axios повертає багато об'єктів, тому вказуємо, лише на потрібний - data!
+};
 
 
 
