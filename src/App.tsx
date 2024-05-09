4713ebccf665a27457ef8151edc45bc933f864d9
DenisOsetrov <dvosetrov@gmail.com>
2024-05-10 00:32:32 +0300

    hw-2, 2 part users-user-post

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,33 @@ const App: FC = () => {
     }
 
     return (
-        <>
-            <UsersComponent choseUser={choseUser}/>
+        <div className={'task'}>
+            <h2>Завдання 2.</h2>
+            <ul>
+            <li> Отримати та вивести інфу по всіх юзерах з https://dummyjson.com/docs/users.</li>
+            <li>Для запитів використовуємо axios та розносимо логіку в сервіси.</li>
+            <li>Структура компонентів : App - Users- User.</li>
+            <li>В компоненті User зробити <button>кнопку</button>, при натсиканні на яку</li>
+            <li>відбувається стейт ліфтінг ід користувача в батьківську компоненту.</li>
+            <li>В батьківській компоненті вивести всі пости обраного юзера</li>
+            <li>(https://dummyjson.com/docs/posts  Get all posts by user id)</li>
+            </ul>
+            <div className={'wrapperApp'}>
 
-            {
-                !!userId && <PostsComponent userId={userId}/>
-            }
+                <div className={'users-box'}>
+                    <h2>Users by API dummyjson.com</h2>
+                    <UsersComponent choseUser={choseUser}/>
+                </div>
 
-        </>
+                <div className={'posts-box'}>
+                    <h2>Posts by user id</h2>
+                    {
+                        !!userId && <PostsComponent userId={userId}/>
+                    }
+                </div>
+
+            </div>
+        </div>
     );
 }
 
--- src/components/user-component/UserComponent.tsx
+++ src/components/user-component/UserComponent.tsx
@@ -3,60 +3,63 @@ import {UserBasicModel} from "../../models/basic-models/UserBasicModel";
 
 interface IProps {
     user: UserBasicModel;
-    choseUser: (id:number) => void;
+    choseUser: (id: number) => void;
 }
-const UserComponent: FC <IProps> = ({user,choseUser}) => {
+
+const UserComponent: FC<IProps> = ({user, choseUser}) => {
     return (
         <div>
             <h4>ID: {user.id}</h4>
-            <p>First Name: {user.firstName}</p>
-            <p>Last Name: {user.lastName}</p>
-            <p>Maiden Name: {user.maidenName}</p>
-            <p>Age: {user.age}</p>
-            <p>Gender: {user.gender}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone: {user.phone}</p>
-            <p>Username: {user.username}</p>
-            <p>Password: {user.password}</p>
-            <p>Birth Date: {user.birthDate}</p>
-            <p>Image: {user.image}</p>
-            <p>Blood Group: {user.bloodGroup}</p>
-            <p>Height: {user.height}</p>
-            <p>Weight: {user.weight}</p>
-            <p>Eye Color: {user.eyeColor}</p>
-            <p>Hair Color: {user.hair.color}</p>
-            <p>Hair Type: {user.hair.type}</p>
-            <p>Domain: {user.domain}</p>
-            <p>IP: {user.ip}</p>
-            <p>Address: {user.address.address}</p>
-            <p>City: {user.address.city}</p>
-            <p>Latitude: {user.address.coordinates.lat}</p>
-            <p>Longitude: {user.address.coordinates.lng}</p>
-            <p>Postal Code: {user.address.postalCode}</p>
-            <p>State: {user.address.state}</p>
-            <p>MAC Address: {user.macAddress}</p>
-            <p>University: {user.university}</p>
-            <p>Card Expire: {user.bank.cardExpire}</p>
-            <p>Card Number: {user.bank.cardNumber}</p>
-            <p>Card Type: {user.bank.cardType}</p>
-            <p>Currency: {user.bank.currency}</p>
-            <p>IBAN: {user.bank.iban}</p>
-            <p>Company Address: {user.company.address.address}</p>
-            <p>Company City: {user.company.address.city}</p>
-            <p>Company Latitude: {user.company.address.coordinates.lat}</p>
-            <p>Company Longitude: {user.company.address.coordinates.lng}</p>
-            <p>Company Postal Code: {user.company.address.postalCode}</p>
-            <p>Company State: {user.company.address.state}</p>
-            <p>Company Department: {user.company.department}</p>
-            <p>Company Name: {user.company.name}</p>
-            <p>Company Title: {user.company.title}</p>
-            <p>EIN: {user.ein}</p>
-            <p>SSN: {user.ssn}</p>
-            <p>User Agent: {user.userAgent}</p>
-            <span><button onClick={() => {
-                choseUser(user.id);
-            }}>show posts this user
+            <ul>
+                <li>First Name: {user.firstName}</li>
+                <li>Last Name: {user.lastName}</li>
+                <li>Maiden Name: {user.maidenName}</li>
+                <li>Age: {user.age}</li>
+                <li>Gender: {user.gender}</li>
+                <li>Email: {user.email}</li>
+                <li>Phone: {user.phone}</li>
+                <li>Username: {user.username}</li>
+                <li>Password: {user.password}</li>
+                <li>Birth Date: {user.birthDate}</li>
+                <li>Image: <img src={user.image} alt={user.firstName}/></li>
+                <li>Blood Group: {user.bloodGroup}</li>
+                <li>Height: {user.height}</li>
+                <li>Weight: {user.weight}</li>
+                <li>Eye Color: {user.eyeColor}</li>
+                <li>Hair Color: {user.hair.color}</li>
+                <li>Hair Type: {user.hair.type}</li>
+                <li>Domain: {user.domain}</li>
+                <li>IP: {user.ip}</li>
+                <li>Address: {user.address.address}</li>
+                <li>City: {user.address.city}</li>
+                <li>Latitude: {user.address.coordinates.lat}</li>
+                <li>Longitude: {user.address.coordinates.lng}</li>
+                <li>Postal Code: {user.address.postalCode}</li>
+                <li>State: {user.address.state}</li>
+                <li>MAC Address: {user.macAddress}</li>
+                <li>University: {user.university}</li>
+                <li>Card Expire: {user.bank.cardExpire}</li>
+                <li>Card Number: {user.bank.cardNumber}</li>
+                <li>Card Type: {user.bank.cardType}</li>
+                <li>Currency: {user.bank.currency}</li>
+                <li>IBAN: {user.bank.iban}</li>
+                <li>Company Address: {user.company.address.address}</li>
+                <li>Company City: {user.company.address.city}</li>
+                <li>Company Latitude: {user.company.address.coordinates.lat}</li>
+                <li>Company Longitude: {user.company.address.coordinates.lng}</li>
+                <li>Company Postal Code: {user.company.address.postalCode}</li>
+                <li>Company State: {user.company.address.state}</li>
+                <li>Company Department: {user.company.department}</li>
+                <li>Company Name: {user.company.name}</li>
+                <li>Company Title: {user.company.title}</li>
+                <li>EIN: {user.ein}</li>
+                <li>SSN: {user.ssn}</li>
+                <li>User Agent: {user.userAgent}</li>
+                <span><button onClick={() => {
+                    choseUser(user.id);
+                }}>show posts this user
             </button></span>
+            </ul>
         </div>
     );
 };
