cb158711e02427bb7ffe937291698e6923a49716
DenisOsetrov <dvosetrov@gmail.com>
2024-05-21 20:33:25 +0300

    hw-6 router: 3 page

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {NavLink, Outlet} from 'react-router-dom';
 import './MainLayout.css';
 import HeaderComponent from "../components/header/HeaderComponent";
 import FooterComponent from "../components/footer/FooterComponent";
 
-const MainLayout = () => {
+const MainLayout: FC = () => {
     return (
         <div>
             <NavLink to="" className="no-decoration">
